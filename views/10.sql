a13bc4fb466bd83cf4dc707073f182b3cd6d8928
Austin Zehnder <134451398+austinzehnder@users.noreply.github.com>
2023-12-19 17:28:45 -0800

    Create 10.sql

---
+++ views/10.sql
@@ -0,0 +1,10 @@
+"""
+write a SQL query to answer a question of your choice about the prints. The query should:
+Make use of AS to rename a column
+Involve at least one condition, using WHERE
+Sort by at least one column, using ORDER BY
+"""
+
+SELECT ROUND(AVG("contrast")) AS "Average Contrast"
+FROM "views"
+WHERE "artist" = 'Hiroshige' ORDER BY "brightness";
