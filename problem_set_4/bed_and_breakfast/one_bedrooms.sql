c0f3060f30f4a592dc5e1ae8f7ea3407a0e551da
Austin Zehnder <134451398+austinzehnder@users.noreply.github.com>
2023-12-19 18:19:16 -0800

    Create one_bedrooms.sql

---
+++ problem_set_4/bed_and_breakfast/one_bedrooms.sql
@@ -0,0 +1,13 @@
+"""
+ write a SQL statement to create a view named one_bedrooms. This view should contain all listings that have 
+exactly one bedroom. Ensure the view contains the following columns:
+id, which is the id of the listing from the listings table.
+property_type, from the listings table.
+host_name, from the listings table.
+accommodates, from the listings table.
+"""
+
+CREATE VIEW "one_bedrooms" AS
+SELECT "id", "prpoerty_type", "host_name", "accomodates"
+FROM "listings"
+WHERE "bedrooms" = 1;
