2ee327a8e0974b4ebff7ebdf6e62ad9aa45208de
Austin Zehnder <134451398+austinzehnder@users.noreply.github.com>
2023-12-19 18:06:09 -0800

    Update 11.sql

--- problem_set_1/moneyball/11.sql
+++ problem_set_1/moneyball/11.sql
@@ -1,10 +1,11 @@
 """
-write a query to return just such a table:
-Your query should return a table with five columns, per the above.
-Order the results, first and foremost, by player’s IDs (least to greatest).
-Order rows about the same player by year, in descending order.
-Consider a corner case: suppose a player has multiple salaries or performances for a given year. Order them first by number of home runs, in descending order, followed by salary, in descending order.
-Be careful to ensure that, for a single row, the salary’s year and the performance’s year match.
+write a SQL query to find the 10 least expensive players per hit in 2001.
+Your query should return a table with three columns, one for the players’ first names, one of their last names, and one called “dollars per hit”.
+You can calculate the “dollars per hit” column by dividing a player’s 2001 salary by the number of hits they made in 2001. Recall you can use AS to rename a column.
+Dividing a salary by 0 hits will result in a NULL value. Avoid the issue by filtering out players with 0 hits.
+Sort the table by the “dollars per hit” column, least to most expensive. If two players have the same “dollars per hit”, order by first name, followed by last name, in alphabetical order.
+As in 10.sql, ensure that the salary’s year and the performance’s year match.
+You may assume, for simplicity, that a player will only have one salary and one performance in 2001.
 """
 
 SELECT "pl"."first_name" AS "first name", "pl"."last_name" AS "last name", ("s"."salary"/"pe"."h") AS "dollars per hit"
